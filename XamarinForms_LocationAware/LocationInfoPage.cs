0da345d797611340a3518a52880438038b8ea2cc
Patrick Tibble <ptibble88@gmail.com>
2019-05-25 00:09:36 -0500

    Updates to dialog service (incomplete) and view models
    LocationInfoPageViewModel to handle adding saved locations
    Converter to handle ListView ItemTapped Event

---
+++ XamarinForms_LocationAware/LocationInfoPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamarinForms_LocationAware
+{
+    public class LocationInfoPage : ContentPage
+    {
+        public LocationInfoPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
