411bae4caa4024620df9624b88cdaa84d33b5d0a
Joseph Anger <jsphanger@yahoo.com>
2019-05-25 11:08:37 -0500

    Styling Updates
    - Added display labels for Lat and Long
    - Added IsVisible binding for remove button
    - Reskinned the mainpage and locationinformation page

--- XamarinForms_LocationAware/ViewModels/LocationInfoPageViewModel.cs
+++ XamarinForms_LocationAware/ViewModels/LocationInfoPageViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Windows.Input;
+using Rg.Plugins.Popup.Services;
+using Xamarin.Forms;
 using XamarinForms_LocationAware.Models;
 using XamarinForms_LocationAware.Services.DataSource.SavedLocations;
 
@@ -9,6 +12,16 @@ namespace XamarinForms_LocationAware.ViewModels
     {
         private LocationModel _location = new LocationModel();
         public LocationModel Location { get => _location; set { _location = value; OnPropertyChanged(nameof(Location)); } }
+
+        public string LatitudeDisplay { get { return "Latitude: " + _location.Latitude; } }
+        public string LongitudeDisplay { get { return "Longitude: " + _location.Longitude; } }
+        public bool EditMode { get { return !String.IsNullOrEmpty(_location.Name); } }
+
+        public ICommand CloseModalCommand => new Command(() =>
+        {
+            // Close the current modal window
+            PopupNavigation.Instance.PopAsync();
+        });
 
         public LocationInfoPageViewModel()
         {
