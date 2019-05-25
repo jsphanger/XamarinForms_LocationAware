6df6dc4dd0be1803d8955881d2404037c907fb1e
Patrick Tibble <ptibble88@gmail.com>
2019-05-24 23:14:23 -0500

    Added mock data from view models to reflect in the views

---
+++ XamarinForms_LocationAware/Models/LocationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinForms_LocationAware.Models
+{
+    public class LocationModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ XamarinForms_LocationAware/Services/DataSource/SavedLocations/ISavedLocationsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinForms_LocationAware.Models;
+
+namespace XamarinForms_LocationAware.Services.DataSource.SavedLocations
+{
+    public interface ISavedLocationsRepository
+    {
+        IList<LocationModel> GetSavedLocations();
+        void SaveLocation(LocationModel model);
+    }
+}
