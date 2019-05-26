58c9142fdfed3e18e1ebef7c0fbfaedbdbc3484c
Joseph Anger <jsphanger@yahoo.com>
2019-05-26 07:03:15 -0500

    Added error handling logic
    - Display modal for people who have permission to location services off or capture general errors with retrieving coordinates.

--- XamarinForms_LocationAware/ViewModels/MainPageViewModel.cs
+++ XamarinForms_LocationAware/ViewModels/MainPageViewModel.cs
@@ -21,61 +21,78 @@ namespace XamarinForms_LocationAware.ViewModels
         {
             // when user taps 'add' button
             // prompt user to input a name
-            var request = new GeolocationRequest(GeolocationAccuracy.Best);
-            var location = await Geolocation.GetLocationAsync(request);
-
-            if (location == null)
+            try
             {
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location == null)
+                {
                     var lastKnown = await Geolocation.GetLastKnownLocationAsync();
                     location = lastKnown == null ? new Location() { Latitude = 0, Longitude = 0 } : await Geolocation.GetLastKnownLocationAsync();
-            }
+                }
 
-            LocationModel loc = new LocationModel() { Latitude = location.Latitude, Longitude = location.Longitude };
-            await PopupNavigation.Instance.PushAsync(new LocationInformation(loc));
+                LocationModel loc = new LocationModel() { Latitude = location.Latitude, Longitude = location.Longitude };
+                await PopupNavigation.Instance.PushAsync(new LocationInformation(loc));
+            }
+            catch(PermissionException)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Could not access the GPS location features.  Please enable permissions for this app in your device settings.", "OK");
+            }
+            catch(Exception)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Could not retreive the location of this device at this time.", "OK");
+            }
 
         });
 
         public ICommand CheckInCommand => new Command(async () =>
         {
-            // when user taps 'check in' button check to see if we are near any of our saved locations
-            LocationModel closestLocation = new LocationModel();
-            var request = new GeolocationRequest(GeolocationAccuracy.Best);
-            var location = await Geolocation.GetLocationAsync(request);
+            try 
+            { 
+                // when user taps 'check in' button check to see if we are near any of our saved locations
+                LocationModel closestLocation = new LocationModel();
+                var request = new GeolocationRequest(GeolocationAccuracy.Best);
+                var location = await Geolocation.GetLocationAsync(request);
 
-            if (location == null)
-            {
-                var lastKnown = await Geolocation.GetLastKnownLocationAsync();
-                location = lastKnown == null ? new Location() { Latitude = 0, Longitude = 0 } : await Geolocation.GetLastKnownLocationAsync();
-            }
-
-            double bestMillage = 0.0;
-            foreach(var local in StoredLocations)
-            {
-                var localLoc = new Location() { Latitude = local.Latitude, Longitude = local.Longitude };
-                var miles = Location.CalculateDistance(location, localLoc, DistanceUnits.Miles);
-
-                if(miles < bestMillage || bestMillage == 0.0)
+                if (location == null)
                 {
-                    bestMillage = miles;
-                    closestLocation = local;
+                    var lastKnown = await Geolocation.GetLastKnownLocationAsync();
+                    location = lastKnown == null ? new Location() { Latitude = 0, Longitude = 0 } : await Geolocation.GetLastKnownLocationAsync();
+                }
+
+                double bestMillage = 0.0;
+                foreach(var local in StoredLocations)
+                {
+                    var localLoc = new Location() { Latitude = local.Latitude, Longitude = local.Longitude };
+                    var miles = Location.CalculateDistance(location, localLoc, DistanceUnits.Miles);
+
+                    if(miles < bestMillage || bestMillage == 0.0)
+                    {
+                        bestMillage = miles;
+                        closestLocation = local;
+                    }
+                }
+
+                //if you are within a quarter mile radius of any location it will check you in.
+                if(StoredLocations.Count == 0){
+                    await Application.Current.MainPage.DisplayAlert("Locations", "You are not within the check-in radius of any saved locations.  To record a new location press the target icon in the top right corner!", "OK");
+                }
+                else if(bestMillage >= 0.0 && bestMillage < 0.25){
+                    await Application.Current.MainPage.DisplayAlert("Locations", String.Format("You are {0} miles from {1} and have been automatically checked in.", bestMillage.ToString("0.##"), closestLocation.Name ), "OK");
+                }
+                else{
+                    //not near anything
+                    await Application.Current.MainPage.DisplayAlert("Locations", "You are not within the check-in radius of any saved locations.", "OK");
                 }
             }
-
-            //if you are within a quarter mile radius of any location it will check you in.
-            if(StoredLocations.Count == 0)
+            catch (PermissionException)
             {
-                await Application.Current.MainPage.DisplayAlert("Locations", "You are not within the check-in radius of any saved locations.  To record a new location press the target icon in the top right corner!", "OK");
+                await Application.Current.MainPage.DisplayAlert("Error", "Could not access the GPS location features.  Please enable permissions for this app in your device settings.", "OK");
             }
-            else if(bestMillage >= 0.0 && bestMillage < 0.25)
+            catch (Exception)
             {
-                await Application.Current.MainPage.DisplayAlert("Locations", 
-                                                                String.Format("You are {0} miles from {1} and have been automatically checked in.", bestMillage.ToString("0.##"), closestLocation.Name ), 
-                                                                "OK");
-            }
-            else
-            {
-                //not near anything
-                await Application.Current.MainPage.DisplayAlert("Locations", "You are not within the check-in radius of any saved locations.", "OK");
+                await Application.Current.MainPage.DisplayAlert("Error", "Could not retreive the location of this device at this time.", "OK");
             }
         });
 
